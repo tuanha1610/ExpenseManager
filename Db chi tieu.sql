60b4555916d8846175074576018baee945e9a0d2
tuanha1610 <tuanha1610@gmail.com>
2013-12-30 23:14:19 +0700

    db
    database of Expense Manager

---
+++ Db chi tieu.sql
@@ -0,0 +1,26 @@
+create database dbExpense
+go
+use dbExpense
+go
+
+create table tblUser(
+idUser int identity(1,1) primary key,
+name varchar(100) not null,
+_user varchar(20) not null,
+_pass varchar(100)not null
+)
+go
+create table tblExpense(
+	 idEx int identity(1,1) primary key,
+	 idUser int not null foreign key  references tblUser(idUser),
+	 _date datetime not null,
+	 price float not null,
+	 _status bit
+)
+go 
+create table tblDetail(
+	idDt int identity(1,1) primary key,
+	nameProduct varchar(100),
+
+
+)
